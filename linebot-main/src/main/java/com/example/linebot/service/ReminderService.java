adc31bf542dcef74c781307d5c31b1206f9378ba
SunaharaKana <cist.b220.k.sunahara@gmail.com>
2022-05-01 11:11:52 +0900

    返答やデータベース登録の役割をもつサービスクラスを作成した

---
+++ linebot-main/src/main/java/com/example/linebot/service/ReminderService.java
@@ -0,0 +1,23 @@
+package com.example.linebot.service;
+
+
+import com.example.linebot.replier.RemindOn;
+import com.example.linebot.value.ReminderSlot;
+import com.example.linebot.value.Reminderltem;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+
+public class ReminderService {
+
+    public RemindOn doReplyOfNewItem(MessageEvent<TextMessageContent> event){
+        String userId = event.getSource().getUserId();
+        TextMessageContent tmc = event.getMessage();
+        String text = tmc.getText();
+        ReminderSlot slot = new ReminderSlot(text);
+        Reminderltem item = new Reminderltem(userId,slot);
+
+
+        return new RemindOn(text);
+    }
+}
