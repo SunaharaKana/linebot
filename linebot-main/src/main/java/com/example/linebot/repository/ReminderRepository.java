379528f52086d4d38b3321291bdc7f93c0bc4da7
SunaharaKana <cist.b220.k.sunahara@gmail.com>
2022-05-02 12:28:01 +0900

    サービスとリポジトリをつなぐ部分を作成した

--- linebot-main/src/main/java/com/example/linebot/repository/ReminderRepository.java
+++ linebot-main/src/main/java/com/example/linebot/repository/ReminderRepository.java
@@ -5,6 +5,7 @@ import com.example.linebot.value.ReminderSlot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import com.example.linebot.repository.ReminderRepository;
 
 @Repository
 public class ReminderRepository {
--- linebot-main/src/main/java/com/example/linebot/service/ReminderService.java
+++ linebot-main/src/main/java/com/example/linebot/service/ReminderService.java
@@ -2,16 +2,25 @@ package com.example.linebot.service;
 
 
 import com.example.linebot.replier.RemindOn;
+import com.example.linebot.repository.ReminderRepository;
 import com.example.linebot.value.ReminderSlot;
 import com.example.linebot.value.Reminderltem;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import com.example.linebot.repository.ReminderRepository;
 
 @Service
 public class ReminderService {
+
+    private final ReminderRepository repository;
+
+    @Autowired
+    public ReminderService(ReminderRepository reminderRepository){
+        this.repository = reminderRepository;
+    }
 
     public RemindOn doReplyOfNewItem(MessageEvent<TextMessageContent> event){
         String userId = event.getSource().getUserId();
@@ -20,6 +29,7 @@ public class ReminderService {
         ReminderSlot slot = new ReminderSlot(text);
         Reminderltem item = new Reminderltem(userId,slot);
 
+        repository.insert(item);
 
         return new RemindOn(text);
     }
