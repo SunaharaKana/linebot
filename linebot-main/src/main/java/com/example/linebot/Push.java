87389e52810b9dd730938af0657f01699f01d154
SunaharaKana <cist.b220.k.sunahara@gmail.com>
2022-05-04 00:04:04 +0900

    Pushクラスを新たなControllerになるよう変更した

--- linebot-main/src/main/java/com/example/linebot/Push.java
+++ linebot-main/src/main/java/com/example/linebot/Push.java
@@ -1,17 +1,59 @@
 package com.example.linebot;
 
+import com.linecorp.bot.model.response.BotApiResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
-@RestController
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.PushMessage;
+import com.linecorp.bot.model.message.TextMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.ExecutionException;
+
+
+@RestController // <1>
 public class Push {
+
+    private static final Logger log = LoggerFactory.getLogger(Push.class);
+
+    // push先のユーザID
+    private String userId = "******";
+
+    private final LineMessagingClient messagingClient;
+
+    @Autowired
+    public Push(LineMessagingClient lineMessagingClient){
+        this.messagingClient = lineMessagingClient;
+    }
 
     // テスト
     @GetMapping("test")
     public String hello(HttpServletRequest request) {
         return "Get from " + request.getRequestURL();
+    }
+
+    // 時報をpushする
+    @GetMapping("timetone") //<2>
+    public String pushTimeTone(){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("a K:mm"); // <3>
+        String text = dtf.format(LocalDateTime.now());
+        try {
+            PushMessage pMsg =
+                    new PushMessage(userId,new TextMessage(text)); // <4>
+            BotApiResponse resp = messagingClient.pushMessage(pMsg).get();
+            log.info("Sent messages: {}",resp);
+        }catch (InterruptedException | ExecutionException e){
+            throw new RuntimeException(e);
+        }
+        return text; // <5>
     }
 
 }
