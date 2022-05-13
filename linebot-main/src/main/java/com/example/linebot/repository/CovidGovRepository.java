56fb333ae064b46ad4b4d8db854dc5cac42dfc43
SunaharaKana <cist.b220.k.sunahara@gmail.com>
2022-05-13 23:17:33 +0900

    itemListの配列をデシリアライズするRepositoryを追加した

---
+++ linebot-main/src/main/java/com/example/linebot/repository/CovidGovRepository.java
@@ -0,0 +1,26 @@
+package com.example.linebot.repository;
+
+import com.example.linebot.value.CovidItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.client.RestTemplate;
+
+public class CovidGovRepository {
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public CovidGovRepository(RestTemplateBuilder templateBuilder){
+        this.restTemplate = templateBuilder.build();
+    }
+
+    public CovidItem findCovidGocAPI(String region){
+        String url = String.format(
+                "https://opendata.corona.go.jp/api/Covid19JapanAll?dataName=%s",
+                region); //(1)
+        CovidItem covidItem =
+                restTemplate.getForObject(url,CovidItem.class); //(2)
+        return covidItem;
+    }
+}
